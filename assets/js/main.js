78e4820216cb324ac4a7b52b185716521770d5ae
Eliyah2 <Eliyah.Impelmans@Student.Gildeopleidingen.nl>
2026-09-22 13:05:08 +0200

    Backend-URL bijgewerkt naar nieuwste deployment

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
   "use strict";
 
   // === VUL IN: jouw Google Apps Script web-app URL (voor het lead-formulier) ===
-  var LEAD_GAS_URL = "https://script.google.com/macros/s/AKfycbzWPoe8aoStJ-lzgUvafzEZyal48CFRmQwj3rptBCzFCVVZvSt_YogooWQdH08fddEA5A/exec";
+  var LEAD_GAS_URL = "https://script.google.com/macros/s/AKfycbzh5cAhz6R3ujC95aLzheVJlkve2DyND7j-oVufkUmwLRQ6bR5HtCBhhF2tahEFqhOrDA/exec";
   // =========================================================================
 
   document.addEventListener("DOMContentLoaded", function () {
